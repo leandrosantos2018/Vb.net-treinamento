241dcbb6f3ce7dd200cd4a957c88289af4536ff0
Leandro Santos <leandrolrs2021@outlook.com>
2023-01-22 17:34:14 -0300

    trabalhando com Mysql

--- cadastro/Context.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports MySql.Data.MySqlClient
-
-Public Class Context
-
-    Dim conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
-
-End Class
--- cadastro/Form1.vb
+++ cadastro/Form1.vb
@@ -1,9 +1,10 @@
 ﻿
 Imports MySql.Data.MySqlClient
+Imports System.Threading
 
 
 Public Class Form1
-
+    Private nr As Thread
 
     Private conexao As MySqlConnection
     Private comando As MySqlCommand
@@ -11,136 +12,134 @@ Public Class Form1
     Private dr As MySqlDataReader
     Private strSQL As String
 
-    Private Sub btnNovo_Click(sender As Object, e As EventArgs) Handles btnNovo.Click
+    'Private Sub btnNovo_Click(sender As Object, e As EventArgs)
 
-        Try
+    '    Try
 
-            conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
-
-
-            strSQL = "insert into cadcliente (nome,numero) values(@Nome, @Numero)"
-
-            comando = New MySqlCommand(strSQL, conexao)
-            comando.Parameters.AddWithValue("@Nome", txtNome.Text)
-            comando.Parameters.AddWithValue("@Numero", txtnumero.Text)
-
-            conexao.Open()
-            comando.ExecuteNonQuery()
+    '        conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
 
 
-        Catch ex As Exception
+    '        strSQL = "insert into cadcliente (nome,numero) values(@Nome, @Numero)"
 
-            MessageBox.Show(ex.Message)
-        Finally
-            comando.Clone()
-            conexao = Nothing
-            comando = Nothing
-            txtNome.Text = Nothing
-            txtId.Text = Nothing
-            txtnumero.Text = Nothing
-        End Try
+    '        comando = New MySqlCommand(strSQL, conexao)
+    '        comando.Parameters.AddWithValue("@Nome", txtNome.Text)
+    '        comando.Parameters.AddWithValue("@Numero", txtnumero.Text)
 
-        carregargrid()
-    End Sub
-
-    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
-
-        Try
-            conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
-
-            strSQL = "update cadcliente set nome = @Nome, numero = @Numero where id = @Id"
-
-            comando = New MySqlCommand(strSQL, conexao)
-            comando.Parameters.AddWithValue("@Id", txtId.Text)
-            comando.Parameters.AddWithValue("@Nome", txtNome.Text)
-            comando.Parameters.AddWithValue("@Numero", txtnumero.Text)
-
-            conexao.Open()
-            comando.ExecuteNonQuery()
+    '        conexao.Open()
+    '        comando.ExecuteNonQuery()
 
 
-        Catch ex As Exception
+    '    Catch ex As Exception
 
-            MessageBox.Show(ex.Message)
-        Finally
-            comando.Clone()
-            conexao = Nothing
-            comando = Nothing
-            txtNome.Text = Nothing
-            txtId.Text = Nothing
-            txtnumero.Text = Nothing
-        End Try
+    '        MessageBox.Show(ex.Message)
+    '    Finally
+    '        comando.Clone()
+    '        conexao = Nothing
+    '        comando = Nothing
+    '        txtNome.Text = Nothing
+    '        txtId.Text = Nothing
+    '        txtnumero.Text = Nothing
+    '    End Try
 
-    End Sub
+    '    carregargrid()
+    'End Sub
 
-    Private Sub btnExcluir_Click(sender As Object, e As EventArgs) Handles btnExcluir.Click
-        Try
-            conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
+    'Private Sub btnEdit_Click(sender As Object, e As EventArgs)
 
-            strSQL = "delete from cadcliente where id=@Id or nome = @Nome or numero = @Numero"
+    '    Try
+    '        conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
 
-            comando = New MySqlCommand(strSQL, conexao)
-            comando.Parameters.AddWithValue("@Id", txtId.Text)
-            comando.Parameters.AddWithValue("@Nome", txtNome.Text)
-            comando.Parameters.AddWithValue("@Numero", txtnumero.Text)
+    '        strSQL = "update cadcliente set nome = @Nome, numero = @Numero where id = @Id"
 
-            conexao.Open()
-            comando.ExecuteNonQuery()
+    '        comando = New MySqlCommand(strSQL, conexao)
+    '        comando.Parameters.AddWithValue("@Id", txtId.Text)
+    '        comando.Parameters.AddWithValue("@Nome", txtNome.Text)
+    '        comando.Parameters.AddWithValue("@Numero", txtnumero.Text)
 
-
-
+    '        conexao.Open()
+    '        comando.ExecuteNonQuery()
 
 
-        Catch ex As Exception
+    '    Catch ex As Exception
 
-            MessageBox.Show(ex.Message)
-        Finally
-            comando.Clone()
-            conexao = Nothing
-            comando = Nothing
-            txtNome.Text = Nothing
-            txtId.Text = Nothing
-            txtnumero.Text = Nothing
-        End Try
-        carregargrid()
+    '        MessageBox.Show(ex.Message)
+    '    Finally
+    '        comando.Clone()
+    '        conexao = Nothing
+    '        comando = Nothing
+    '        txtNome.Text = Nothing
+    '        txtId.Text = Nothing
+    '        txtnumero.Text = Nothing
+    '    End Try
+
+    'End Sub
+
+    'Private Sub btnExcluir_Click(sender As Object, e As EventArgs)
+    '    Try
+    '        conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
+
+    '        strSQL = "delete from cadcliente where id=@Id or nome = @Nome or numero = @Numero"
+
+    '        comando = New MySqlCommand(strSQL, conexao)
+    '        comando.Parameters.AddWithValue("@Id", txtId.Text)
+    '        comando.Parameters.AddWithValue("@Nome", txtNome.Text)
+    '        comando.Parameters.AddWithValue("@Numero", txtnumero.Text)
+
+    '        conexao.Open()
+    '        comando.ExecuteNonQuery()
 
 
 
 
-    End Sub
+
+    '    Catch ex As Exception
+
+    '        MessageBox.Show(ex.Message)
+    '    Finally
+    '        comando.Clone()
+    '        conexao = Nothing
+    '        comando = Nothing
+    '        txtNome.Text = Nothing
+    '        txtId.Text = Nothing
+    '        txtnumero.Text = Nothing
+    '    End Try
+    '    carregargrid()
+
+
+
+
+    'End Sub
 
     Private Sub btnConsulta_Click(sender As Object, e As EventArgs) Handles btnConsulta.Click
         Try
-            txtId.Enabled = False
+
             conexao = New MySqlConnection("Server=localhost;Database=crud;Uid=root;Pwd=;")
 
-            strSQL = "select * from cadcliente where id = @Id"
+            strSQL = txtSQL.Text
 
-            comando = New MySqlCommand(strSQL, conexao)
-            comando.Parameters.AddWithValue("@Id", txtId.Text)
+            Dim dt As New DataTable
 
+            da = New MySqlDataAdapter(strSQL, conexao)
 
-            conexao.Open()
-            dr = comando.ExecuteReader()
+            da.Fill(dt)
 
-            Do While dr.Read
-                txtNome.Text = dr("nome")
-                txtnumero.Text = dr("numero")
-            Loop
+            dgGrid.DataSource = dt
+            ' conexao.Open()
+
 
 
         Catch ex As Exception
 
             MessageBox.Show(ex.Message)
         Finally
-            comando.Clone()
+            '  comando.Clone()
             conexao = Nothing
             comando = Nothing
         End Try
 
     End Sub
 
-    Private Sub btnExibir_Click(sender As Object, e As EventArgs) Handles btnExibir.Click
+    Private Sub btnExibir_Click(sender As Object, e As EventArgs)
         carregargrid()
     End Sub
 
